21ea676e278bde3a34aa7bc36f7d2f3f100b8287
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-27 19:10:07 -0300

    :sparkles:

--- 02TheBasics/my-first-app/src/app/app.module.ts
+++ 02TheBasics/my-first-app/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
   ],
   imports: [
     BrowserModule
