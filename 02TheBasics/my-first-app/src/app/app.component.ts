9732af7b2cb8a3dc7faad9ac164e14b7d1720610
Matheus Batista <mbatista.sarti@gmail.com>
2024-04-29 18:43:51 -0300

    :sparkles:

--- 02TheBasics/my-first-app/src/app/app.component.ts
+++ 02TheBasics/my-first-app/src/app/app.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  styles: [`
+    h3 {color: dodgerblue;
+    }
+  `]
 })
 export class AppComponent {
   title = 'my-first-app';
