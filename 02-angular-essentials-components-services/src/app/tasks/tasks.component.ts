f56bab96c650f9195938f470be1d150b55729884
Matheus Batista <mbatista.sarti@gmail.com>
2024-06-16 20:29:45 -0300

    :sparkles: inject tasks service to components

--- 02-angular-essentials-components-services/src/app/tasks/tasks.component.ts
+++ 02-angular-essentials-components-services/src/app/tasks/tasks.component.ts
@@ -1,56 +1,39 @@
-import {Component, Input} from '@angular/core';
-import {TaskComponent} from "./task/task.component";
-import {User} from "../models/user.model";
-import {Task} from "../models/task.model";
+import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+import { User } from '../models/user.model';
+import { Task } from '../models/task.model';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [
-    TaskComponent
-  ],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.scss'
+  styleUrl: './tasks.component.scss',
 })
 export class TasksComponent {
   @Input() selectedUser!: User;
+  isAddingTask: boolean = false;
+  counter: number = 7;
+
   tasks: Task[] = [];
 
-  ngOnInit(): void {
-    this.initializeTasks();
-    console.log('inicializado');
-  }
-
-  private initializeTasks(): void {
-    this.tasks = [
-      {id: 't1', userId: 'u1', title: 'Master Angular', summary: 'Learn Angular.', dueDate: '2025-12-31'},
-      {
-        id: 't2',
-        userId: 'u2',
-        title: 'Build a React App',
-        summary: 'Create a React application.',
-        dueDate: '2025-10-15'
-      },
-      {id: 't3', userId: 'u3', title: 'Explore Vue.js', summary: 'Dive into Vue.js.', dueDate: '2025-11-30'},
-      {id: 't4', userId: 'u4', title: 'Explore C#', summary: 'Dive into C#.', dueDate: '2025-11-30'},
-      {id: 't5', userId: 'u2', title: 'Master Angular', summary: 'Learn Angular.', dueDate: '2025-12-31'},
-      {
-        id: 't6',
-        userId: 'u1',
-        title: 'Build a React App',
-        summary: 'Create a React application.',
-        dueDate: '2025-10-15'
-      },
-      {id: 't7', userId: 'u2', title: 'Explore Vue.js', summary: 'Dive into Vue.js.', dueDate: '2025-11-30'}
-    ];
-  }
+  constructor(private tasksService: TasksService) {}
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.selectedUser.id);
+    return this.tasksService.getUserTasks(this.selectedUser.id);
   }
 
   onCompleteTask($event: Task) {
-    console.log(this.tasks)
-    this.tasks = this.tasks.filter((task) => task.id !== $event.id);
+    this.tasksService.removeTask($event);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCloseAddTask() {
+    this.isAddingTask = false;
   }
 }
