aad3379cf20adc61c307d5e65109cc69bc13ba4b
Matheus Batista <mbatista.sarti@gmail.com>
2024-06-16 20:30:47 -0300

    :sparkles: minor adjustments

--- 02-angular-essentials-components-services/src/app/tasks/task/task.component.ts
+++ 02-angular-essentials-components-services/src/app/tasks/task/task.component.ts
@@ -1,18 +1,20 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {Task} from "../../models/task.model";
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Task } from '../../models/task.model';
+import { CardComponent } from '../../shared/card/card.component';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.scss'
+  styleUrl: './task.component.scss',
 })
 export class TaskComponent {
   @Input() task!: Task;
-  @Output() completeTask : EventEmitter<Task> = new EventEmitter<Task>();
+  @Output() completeTask: EventEmitter<Task> = new EventEmitter<Task>();
 
-  OnCompleteTask(){
+  OnCompleteTask() {
     this.completeTask.emit(this.task);
   }
 }
