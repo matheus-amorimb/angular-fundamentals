d77dcca2195e6ede342b80baf8e3186f9927b682
Matheus Batista <mbatista.sarti@gmail.com>
2024-06-17 18:10:30 -0300

    :sparkles: create service to handle investments calculation

---
+++ 04-angular-essentials-project/src/app/investment-calculator.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InvestmentCalculatorService {
+
+  calculateInvestmentResults(investmentDetails: any): any[] {
+    const years = investmentDetails.years;
+    const initialInvestment = investmentDetails.initialInvestment;
+    const additionalInvestment = investmentDetails.additionalInvestment;
+    const expectedRate = investmentDetails.expectedRate / 100; // Convert percentage to decimal
+
+    const investmentValues = [];
+    let totalInvested = initialInvestment;
+    let totalInterest = 0;
+
+    for (let year = 1; year <= years; year++) {
+      const interestEarned = totalInvested * expectedRate;
+      totalInterest += interestEarned;
+      totalInvested += additionalInvestment;
+      totalInvested += interestEarned;
+
+      investmentValues.push({
+        year,
+        valueEndOfYear: totalInvested,
+        interest: interestEarned,
+        totalInterest,
+        totalAmountInvested: totalInvested + additionalInvestment * year
+      });
+    }
+
+    return investmentValues;
+  }
+}
