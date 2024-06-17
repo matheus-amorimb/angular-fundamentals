dcac339f4faa2753707dede3c4a65296d8eeee63
Matheus Batista <mbatista.sarti@gmail.com>
2024-06-17 18:10:07 -0300

    :sparkles: generate component to create table of investment

---
+++ 04-angular-essentials-project/src/app/investment-table/investment-table.component.ts
@@ -0,0 +1,19 @@
+import {Component, inject, Input} from '@angular/core';
+import {InvestmentCalculatorService} from "../investment-calculator.service";
+import {CurrencyPipe} from "@angular/common";
+
+@Component({
+  selector: 'app-investment-table',
+  standalone: true,
+  imports: [
+    CurrencyPipe
+  ],
+  templateUrl: './investment-table.component.html',
+  styleUrl: './investment-table.component.scss'
+})
+export class InvestmentTableComponent {
+
+  investmentService = inject(InvestmentCalculatorService);
+  @Input() investmentValues!: any[];
+
+}
