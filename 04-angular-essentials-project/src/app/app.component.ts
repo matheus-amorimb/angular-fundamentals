702c724d8f271e0bc5ead44bd3deb6b503a8fc96
Matheus Batista <mbatista.sarti@gmail.com>
2024-06-17 18:11:31 -0300

    :sparkles: first commit

---
+++ 04-angular-essentials-project/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./header/header.component";
+import {InvestmentDetailsComponent} from "./investment-details/investment-details.component";
+import {InvestmentTableComponent} from "./investment-table/investment-table.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, InvestmentDetailsComponent, InvestmentTableComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = '04-angular-essentials-project';
+  investmentValues! : any
+
+  onInvestmentsCalculated($event: any[]) {
+    this.investmentValues = $event;
+  }
+}
