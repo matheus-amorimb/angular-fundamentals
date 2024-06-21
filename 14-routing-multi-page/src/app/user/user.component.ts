4d2d669875fde9459c99dac169d0ea8a8b26dfc5
Matheus Batista <mbatista.sarti@gmail.com>
2024-06-20 21:57:37 -0300

    :bug: adjust navigation method when selection a button

--- 14-routing-multi-page/src/app/user/user.component.ts
+++ 14-routing-multi-page/src/app/user/user.component.ts
@@ -1,14 +1,14 @@
-import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {NgFor} from '@angular/common';
 import {User} from '../models/user.model';
 import {CardComponent} from '../shared/card/card.component';
-import {Router} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
 import {UserService} from "./user.service";
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [NgFor, CardComponent],
+  imports: [NgFor, CardComponent, RouterLink],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
@@ -31,12 +31,13 @@ export class UserComponent {
   }
 
   onSelectUser(user: User) {
-    this.router.navigate(['/tasks', user.id]);
     if(user.id === this.selectedUser?.id){
       this.toUnselect = !this.toUnselect;
       this.router.navigate([''])
+      this.selectedUser = {} as User;
       return
     }
+    this.router.navigate(['/tasks', user.id]);
     this.selectedUser = user;
     this.toUnselect = false;
 
